213e99a11e8c2aceb5a35d087323a9db31e1f767
JasonYRChen <jkmylife@gmail.com>
2022-01-11 20:34:12 +0800

    (test.c): catalog finish

---
+++ test.c
@@ -0,0 +1,15 @@
+/* This is a space to learn C */
+
+# include <stdio.h> 
+
+int main()
+{
+    char ex[10];
+
+    puts("Enter: ");
+    scanf("%3s", ex);
+    printf("This is your message: %s", ex); 
+
+    return 0;
+}
+
