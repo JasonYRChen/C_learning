b5ec5d78eb7618fe49165788bb0908ccf1600b6d
JasonYRChen <jkmylife@gmail.com>
2022-01-13 16:23:04 +0800

    (card_bet.c): do while loop

--- card_bet.c
+++ card_bet.c
@@ -6,31 +6,38 @@
 int main()
 {
     char card[3];
-    int value;
+    do {
+        int value;
+        
+        puts("Enter your card:");
+        scanf("%2s", card);
 
-    puts("Enter a card:");
-    scanf("%2s", card);
-    
-    switch (card[0]) {
-    case 'k':
-    case 'q':
-    case 'j':
-    case 'a':
-        if (card[1] != '\0')
-            value = 0;
-        else {
-            value = 10;
-            if (card[0] == 'a')
-                value++;
+        switch (card[0]) {
+        case 'k':
+        case 'q':
+        case 'j':
+        case 'a':
+            if (card[1] != '\0')
+                value = 0;
+            else {
+                value = 10;
+                if (card[0] == 'a')
+                    value++;
+            }
+            break;
+        default:
+            value = atoi(card);
+            if (!(0 <= value && value <= 10))
+                value = 0;
         }
-        break;
-    default:
-        value = atoi(card);
-        if ((0 >= value) || (value > 10))
-            value = 0;
-    }
-    
-    printf("You have %i points", value);
+
+        if (card[0] != 'X') {
+            if (!((0 < value) && (value <= 11)))
+                printf("Invalid card.\n");
+            else
+                printf("You got %i points\n", value);
+        }
+    } while (card[0] != 'X');
 
     return 0;
 }
