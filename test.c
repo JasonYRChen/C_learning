ddc64e7b764767a4df10fb91a20280cfdb55d8f0
JasonYRChen <jkmylife@gmail.com>
2022-02-09 09:16:50 +0800

    (test.c): function reuse testing

--- test.c
+++ test.c
@@ -1,53 +1,22 @@
-# include <stdio.h>
-
-int coordinate_check(float coord, float limit)
-{
-    if ((coord > limit) || (coord < -limit))
-        return 0;
-    else
-        return 1;
-}
+#include <stdio.h>
+#include "encrypt_folder/header.h"
 
 int main()
 {
-    float longitude;
-    float latitude;
-    char info[80];
-    int start = 0;
-    int lines = 1;
-    int error = 0;
+    char msg[100];
+    int code;
 
-    fprintf(stdout, "data=[");
-    while (3 == scanf("%f,%f,%79[^\n]", &longitude, &latitude, info)) {
-        int valid_long = coordinate_check(longitude, 180);
-        int valid_lati = coordinate_check(latitude, 90);
+    printf("Enter the message to encrypt: ");
+    scanf("%99[^\n]", msg);
+    printf("Enter the encrypt code: ");
+    scanf("%i", &code);
 
-        if (!valid_long || !valid_lati) {
-            if (!error)
-                error = 1;
+    printf("Original message : %s\n", msg);
+    encrypt(msg, code);
+    printf("Encrypted message: %s\n", msg);
+    descrypt(msg, code);
+    printf("Recovered message: %s\n", msg);
 
-            fprintf(stderr, "Line %i.", lines);
-            if (!valid_long)
-                fprintf(stderr, " longitude: %f", longitude);
-            if (!valid_lati)
-                fprintf(stderr, " latitude: %f", latitude);
-            fprintf(stderr, "\n");
-        } else {
-            if (start)
-                fprintf(stdout, ",\n");
-            else {
-                start = 1;
-                fprintf(stdout, "\n");
-            }
-            fprintf(stdout, "{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
-        }
-        lines++;
-    }
-    printf("]");
-
-    if (error)
-        return 2;
-    else
-        return 0;
+    return 0;
 }
 
