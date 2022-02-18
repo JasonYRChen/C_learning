6d0f4e2ebc93336294ef62d43fd296b9e02464c1
JasonYRChen <jkmylife@gmail.com>
2022-02-18 10:08:21 +0800

    (test.c): correct header path for encryption

--- test.c
+++ test.c
@@ -1,55 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "header.h"
 
-typedef struct island {
-    struct island *prev;
-    struct island *next;
-    char *name;
-} island;
-
-island* add_island(char* name)
+int main()
 {
-    island* new = malloc(sizeof(island));
-    new->prev = NULL;
-    new->next = NULL;
-    new->name = strdup(name);
+    char msg[100];
+    int code;
 
-    return new;
-}
+    printf("Enter the message to encrypt: ");
+    scanf("%99[^\n]", msg);
+    printf("Enter the encrypt code: ");
+    scanf("%i", &code);
 
-int main(int argc, char *argv[])
-{
-    char island_name[80];
-    FILE *input = fopen(argv[1], "r");
-    island head = {NULL, NULL, "HEAD"};
-    island *temp = &head;
+    printf("Original message : %s\n", msg);
+    encrypt(msg, code);
+    printf("Encrypted message: %s\n", msg);
+    descrypt(msg, code);
+    printf("Recovered message: %s\n", msg);
 
-    while (fscanf(input, "%[^\n]\n", island_name) == 1) {
-        temp->next = add_island(island_name);
-        temp->next->prev = temp;
-        temp = temp->next;
-    }
-    fclose(input);
-
-    for (temp=&head; temp; temp=temp->next) { 
-        printf("name: %s", temp->name);
-        if (temp->prev)
-            printf(", prev: %s", temp->prev->name);
-        if (temp->next)
-            printf(", next: %s", temp->next->name);
-        printf("\n");
-    }
-
-    temp = head.next;
-    island* temp2;
-    while (temp) {
-        free(temp->name);
-        temp2 = temp->next;
-        free(temp);
-        temp = temp2;
-    }
- 
     return 0;
 }
 
