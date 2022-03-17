f232967430deac28fa6f4c7f6f04a0d788074d66
JasonYRChen <jkmylife@gmail.com>
2022-03-17 14:48:36 +0800

    (test.c): thread safe coding

--- test.c
+++ test.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <arpa/inet.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+
+int BEERS = 100000;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void error(char *msg)
 {
@@ -12,35 +14,34 @@ void error(char *msg)
     exit(1);
 }
 
-int main ()
+void* drink(void *upper_limit)
 {
-    char *advice[] = {
-        "Take smaller bites\r\n", 
-        "Go for the tight jeans. No they do NOT make you look fat.\r\n",
-        "One word: inappropriate\r\n",
-        "Just for today, be honest. Tell your boss what your *really* think\r\n",
-        "You might want to rethink that haircut\r\n"
-    };
-    int listener_d = socket(PF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in name;
-    name.sin_family = PF_INET;
-    name.sin_port = (in_port_t)htons(30000);
-    name.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(listener_d, (struct sockaddr *)&name, sizeof(name)) == -1)
-        error("Bind failed");
-    if (listen(listener_d, 10) == -1)
-        error("Listen failed");
-    puts("Waiting for connection");
-    while (1) {
-        struct sockaddr_storage client_addr;
-        unsigned int address_size = sizeof(client_addr);
-        int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
-        char *msg = advice[rand() % 5];
-        if (send(connect_d, msg, strlen(msg), 0) == -1)
-            error("Send message failed");
-        close(connect_d);
+    for (int i=0; i<(long)upper_limit; i++) {
+        pthread_mutex_lock(&lock);
+        BEERS--;
+        pthread_mutex_unlock(&lock);
     }
+    return NULL;
+}
+
+int main()
+{
+    int persons = 10;
+    long upper_limit = BEERS / persons;
+    pthread_t threads[persons];
+    
+    for (int i=0; i<persons; i++) {
+        if (pthread_create(&threads[i], NULL, drink, (void*)upper_limit) == -1)
+            error("Cannot create thread");
+    }
+
+    void *result;
+    for (int i=0; i<10; i++) {
+        if (pthread_join(threads[i], result) == -1)
+            error("Cannot join thread");
+    }
+    
+    printf("%i beers left\n", BEERS);
 
     return 0;
 }
